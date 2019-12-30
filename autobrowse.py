5601a9b7e8fa6b37854e6223443a6707f7203211
Parshwas <parshwa.surat@gmail.com>
2019-12-30 20:18:21 +0530

    Added Firefox

--- autobrowse.py
+++ autobrowse.py
@@ -8,15 +8,14 @@ import contextlib
 wait_time = 5
 
 def create_driver():
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options = webdriver.FirefoxOptions()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--log-level=3")
 
     with contextlib.redirect_stdout(None):
-        driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+        driver = webdriver.Firefox(options=chrome_options)
     
     return driver
 
